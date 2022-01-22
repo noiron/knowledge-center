ef5c83d1a49c1d6cdb27b2c977d50637317dc936
wu kai <wukai2048@gmail.com>
2022-01-22 17:56:44 +0800

    为 styled-components 配置 plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,31 @@
-import { defineConfig } from 'vite'
-import path from 'path'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          [
+            // https://github.com/styled-components/babel-plugin-styled-components/issues/350#issuecomment-979873241
+            'babel-plugin-styled-components',
+            {
+              displayName: true,
+              fileName: false,
+            },
+          ],
+        ],
+      },
+    }),
+  ],
   server: {
     proxy: {
       '/api': {
-        target: "http://localhost:4001",
+        target: 'http://localhost:4001',
         changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
       // "^/.+\.(?:jpe?g|png)": {
       //   target: "http://localhost:4001",
@@ -20,7 +35,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, "src"),
-    }
-  }
-})
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});
