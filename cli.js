135ee89be7e84f4845cf29fcb79fc6c00915964a
wu kai <wukai2048@gmail.com>
2022-01-27 19:41:49 +0800

    update cli.js

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 // 写这个脚本的原因是 server 需要使用 ts-node 开启，但是现在还不知道如何在 .sh 脚本中处理绝对路径
-const exec = require('child_process').exec;
+const { fork } = require('child_process');
 const path = require('path');
 
 const serverPath = path.resolve(__dirname, './server/index.ts');
-exec(`ts-node ${serverPath}`);
+fork(serverPath);
