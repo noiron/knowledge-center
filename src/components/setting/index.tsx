a68b5c9c4a1af52c1f79596da9f7ed21f11ff30d
wu kai <wukai2048@gmail.com>
2022-03-06 13:51:26 +0800

    修改样式文件的导入方式

--- src/components/setting/index.tsx
+++ src/components/setting/index.tsx
@@ -3,6 +3,8 @@
  */
 import styled from 'styled-components';
 import Drawer from '@mui/material/Drawer';
+import { extractFileName } from '@/utils';
+import { useEffect, useState } from 'react';
 // import Box from '@mui/material/Box';
 
 const StyledSetting = styled.div`
@@ -18,48 +20,79 @@ interface SettingProps {
   onClose: () => void;
 }
 
-const styleFiles1 = ['../../styles/my.less'];
-const styleFiles2 = ['../../styles/bear.css', '../../styles/second.css'];
+const defaultStyles = ['../../styles/default.less'];
+
+/**
+ * 一个加载样式的函数，做两件事：
+ * 1. 加载样式
+ * 2. 将一个 class 名称加到 html 节点上
+ * 
+ * 假设有一个新的样式文件 special.less，则文件内容中的样式需要用 `.special {}` 包裹
+ */
+function loadStyle(styleFilePath: string) {
+  import(styleFilePath);
+  const fileName = extractFileName(styleFilePath);
+  // todo: 换个正则
+  const className = fileName?.split('.')[0] || '';
+  const html = document.querySelector('html');
+  if (html) {
+    html.classList.add(className);
+  }
+}
 
 const Setting = (props: SettingProps) => {
   const { isOpen, onClose } = props;
   const anchor = 'right';
+  const [currentStyleFile, setCurrentStyleFile] = useState(defaultStyles);
+
+  useEffect(() => {
+    const html = document.querySelector('html');
+    if (html) {
+      html.classList.remove(...html.classList);
+    }
+
+    currentStyleFile.forEach((styleFile) => {
+      loadStyle(styleFile);
+    });
+  }, [currentStyleFile]);
 
   return (
     <Drawer anchor={anchor} open={isOpen} onClose={onClose}>
       <StyledSetting>
-        在这里配置选项
         <div>
           <button
             onClick={() => {
-              styleFiles1.forEach((file) => {
-                import(file);
-              });
+              setCurrentStyleFile(defaultStyles);
             }}
           >
-            使用样式1
+            还原默认样式
           </button>
         </div>
         <div>
           <button
             onClick={() => {
-              styleFiles2.forEach((file) => {
-                import(file);
-              });
+              setCurrentStyleFile(['../../styles/bear.css']);
             }}
           >
-            使用样式2
+            使用样式 Bear
           </button>
         </div>
         <div>
           <button
             onClick={() => {
-              ['../../styles/3.css'].forEach((file) => {
-                import(file);
-              });
+              setCurrentStyleFile(['../../styles/second.less']);
             }}
           >
-            使用样式3
+            测试样式：设置引用样式
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              setCurrentStyleFile(['../../styles/style3.less']);
+            }}
+          >
+            测试样式，边框设为红色
           </button>
         </div>
       </StyledSetting>
